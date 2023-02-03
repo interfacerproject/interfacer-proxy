15d5e1e1103f3dab0c8e6903b2ae59903d60d923
srfsh <dev@srf.sh>
2023-02-03 16:21:03 +0300

    logger: fix style and apply go fmt

--- logger/log.go
+++ logger/log.go
@@ -1,26 +1,24 @@
 package logger
 
 import (
-    "os"
-    "path/filepath"
-    "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
+	"os"
+	"path/filepath"
 )
 
 var Log = logrus.New()
 
 func InitLog(logPath string) {
-    Log.SetFormatter(&logrus.JSONFormatter{})
+	Log.SetFormatter(&logrus.JSONFormatter{})
 
-    Log.Out = os.Stdout
+	Log.Out = os.Stdout
 
-    if file, err := os.OpenFile(
-                filepath.Join(logPath, "proxy.log"),
-                os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666);
-            err != nil {
-        Log.WithFields(logrus.Fields{
-            "err": err.Error(),
-        }).Info("Failed to log to file, using default stderr")
-    } else {
-        Log.Out = file
-    }
+	file, err := os.OpenFile(filepath.Join(logPath, "proxy.log"),
+		os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		Log.WithFields(logrus.Fields{"err": err.Error()}).
+			Info("Failed to log to file, using default stderr")
+	} else {
+		Log.Out = file
+	}
 }
